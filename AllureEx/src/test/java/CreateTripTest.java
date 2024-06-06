54fc75413a0f0c5f23eebe3779c92ab5ab94e45f
MrRocketR <kuptsovns@gmail.com>
2024-06-06 17:31:00 +0300

    Задание №3_Allure

--- AllureEx/src/test/java/CreateTripTest.java
+++ AllureEx/src/test/java/CreateTripTest.java
@@ -1,17 +1,7 @@
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import ru.ibs.selenium.training.pages.AllTripsPage;
-import ru.ibs.selenium.training.pages.CreateTripPage;
-import ru.ibs.selenium.training.pages.LoginPage;
-import ru.ibs.selenium.training.pages.QuickLaunchBarPage;
-
-import java.util.Properties;
-
-import static ru.ibs.selenium.training.config.TestConfig.getInstance;
 
 public class CreateTripTest extends BaseSteps{
 
@@ -19,7 +9,7 @@ public class CreateTripTest extends BaseSteps{
     @Story("Простой тест на создание командировки")
     @Description("Авторизация по паролю и попытка создать командировку без заполнения сотрудников")
     @Feature("Создание командировки")
-    public void tripTest() {
+    public void createTripTest() {
         baseAuth();
         pickCreateTrip();
         createDefaultTrip();
